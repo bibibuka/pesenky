7152680efe8dfbe2d901dc07a9c8ebb68e26c2e7
bibibuka <bibibuka@users.noreply.github.com>
2026-05-27 02:44:51 +0300

    Updates

--- assets/photo_2026-05-27_02-32-01-BLDUmUpE.js
+++
@@ -1,1 +0,0 @@
-const o="/pesenky/assets/photo_2026-05-27_02-32-01-BUcaBN72.jpg";export{o as default};
---
+++ assets/photo_2026-05-27_02-41-05-CNRkci5P.js
@@ -0,0 +1,1 @@
+const o="/pesenky/assets/photo_2026-05-27_02-41-05-D1Rd5a0h.jpg";export{o as default};
