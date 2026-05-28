54181063b0d94bd5e46f03259383f36298dc99fc
bibibuka <bibibuka@users.noreply.github.com>
2026-05-28 23:50:55 +0300

    Updates

---
+++ assets/hd_818854c7843a48548ae20e9554fccf34-Bm4qxPjl.js
@@ -0,0 +1,1 @@
+const e="/pesenky/assets/hd_818854c7843a48548ae20e9554fccf34-CIzD90KA.jpg";export{e as default};
--- assets/photo_2026-05-27_02-41-05-CNRkci5P.js
+++
@@ -1,1 +0,0 @@
-const o="/pesenky/assets/photo_2026-05-27_02-41-05-D1Rd5a0h.jpg";export{o as default};
